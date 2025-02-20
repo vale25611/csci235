bc1c0a1448bee0f644f64e586b0f7c768561a935
vale25611 <165075092+vale25611@users.noreply.github.com>
2025-02-20 13:31:23 -0500

    Add files via upload

---
+++ project 5/main.cpp
@@ -0,0 +1,24 @@
+/*
+Valentina Quiroga
+VALENTINA.QUIROGA76@myhunter.cuny.edu
+Tiziana Ligorio
+Project 5
+*/
+
+#include <iostream>
+
+#include "DoublyLinkedList.hpp"
+
+#include "CardShop.cpp"
+
+#include "YGOCard.hpp"
+
+
+int main() {
+  CardShop yogi("cards.csv");
+
+  std::cout << "Number of times cards moved: " << yogi.insertionSort(std::less < int > {}, "atk") << std::endl;
+  yogi.display();
+
+  return 0;
+}
